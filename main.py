c35daae3de075257b30afb51c71b11d61b547bfb
Zokyn <gustavo.casaes@mail.uft.edu.br>
2023-06-07 20:29:41 -0300

    FEAT mudanças na época e escala do gráfico

--- main.py
+++ main.py
@@ -34,7 +34,6 @@ for object_column in X.select_dtypes('object').columns:
     # dataset, e posteriormente o modelo lide apenas com um tipo de dados
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7)
-    # print(X_train.shape, X_test.shape)
 ### MODELING ###
 def build_model():
     n = X_train.shape[1]
@@ -51,28 +50,43 @@ def build_model():
     accuracy = tf.keras.metrics.Accuracy()
     model.compile(loss='mse', optimizer='adam', metrics=['mae','mse', accuracy])
 
-    history = model.fit(X_train, y_train, epochs=45, callbacks=[callbacks])
+    history = model.fit(X_train, y_train, epochs=200, callbacks=[callbacks])
 
     # Predizendo valores de treinamento
-    # e = model.predict(X_test[:10])
+    y_predict = model.predict(X_test)
     # print(X_test[:10]['salary_in_usd'])
     # print(e)
+
+    # plt.scatter(range(len(y_test[:500])), y_test[:500], color='blue', label='real')
+    # plt.scatter(range(len(x_predict[:500])), x_predict[:500],color='red', marker='x', label='predict')
+    # plt.xlabel('epoch')
+    # plt.show()
 
     # Mostrando historico de perdas e metricas
     h = pd.DataFrame(history.history)
     h['epoch'] = history.epoch
     # print(h.tail())
-    return h 
-def show_results(hist):
-    print(f"%"*10)
-    print(f"ÉPOCAS TOTAL {hist['epoch'].iloc[:-1]}")
-    print(f"MAE| {hist['mae'].iloc[:-1]}")
-    # print(f"MAE| {hist.iloc[hist['mae'][:1].index]}")
-    print(f"MSE| {hist['mse'].iloc[:-1]}")
-    print(f"MSE| {hist['accuracy'].iloc[:-1]}")
-    # print(f"{hist.keys()}")
+    return h , X_test, y_predict
+def show_predicts(x_predict, y_predict):
+        # plt.scatter(range(len(y), np.array(y_test['salary_in_usd'][:10]), color='blue', label='real')
+    # plt.scatter(range(len(X_test[:10])), np.array(X_test['salary_in_usd'][:10]) ,color='red', marker='x', label='predict')
+    plt.scatter(len(y_predict), y_predict, color='blue', label='real')
+    plt.scatter((len(x_predict)), x_predict, color='red', marker='x', label='predict')
+    
+    plt.show()
 
-def show_grapichs(hist):
+
+def show_results(hist):
+    print(f"%"*25)
+    print(f"ÉPOCAS TOTAL {hist['epoch'].iloc[-1]}")
+    print(f"MAE| {hist['mae'].iloc[-1]}")
+    # print(f"MAE| {hist.iloc[hist['mae'][:1].index]}")
+    print(f"MSE| {hist['mse'].iloc[-1]}")
+    print(f"Accuracy| {hist['accuracy'].iloc[-1]}")
+    # print(f"{hist.keys()}")
+    print(f"%"*25)
+
+def show_grapichs(hist,):
     plt.figure(figsize=(8, 6))
     # plt.xlabel('Epoch')
     # plt.ylabel('Mean Square Error [$MPG^2$]')
@@ -85,16 +99,15 @@ def show_grapichs(hist):
     plt.xlabel('Epoch')
     plt.ylabel('Mean Abs Error [MPG]')
     plt.plot(hist['epoch'], hist['mae'],
-            label='Train Error')
+            label='MAE')
     plt.plot(hist['epoch'], hist['loss'],
-            label = 'Val Error')
-    plt.ylim([0,5])
+            label = 'Loss Error')
+    plt.ylim([0,100])
     plt.legend()
-    # plt.scatter(range(len(y_test['salary_in_usd'][:10])), np.array(y_test['salary_in_usd'][:10]), color='blue', label='real')
-    # plt.scatter(range(len(X_test[:10])), np.array(X_test['salary_in_usd'][:10]) ,color='red', marker='x', label='predict')
-    # plt.xlabel('epoch')
+
     plt.show()
 
-hist = build_model()
+hist, x, y = build_model()
 show_results(hist)
+# show_predicts(x, y)
 show_grapichs(hist)
